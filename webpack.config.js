f10f86745fda5a3970e9bc0e09606572f35b9353
Adan Mota Cruz <adanmotacruz17@gmail.com>
2025-11-22 11:46:16 -0600

    chore: initial setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    clean: true
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,  
+        exclude: /node_modules/,
+        use: 'babel-loader'    
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+      }
+    ]
+  },
+  devServer: {
+    static: './public',
+    port: 3000,
+    open: true,
+    hot: true,
+  }
+};
