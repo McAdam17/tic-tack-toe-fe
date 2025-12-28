960b022ba438a10bc8a1d56ca0fc62b4ed1fa727
Adan Mota Cruz <adanmotacruz17@gmail.com>
2025-12-28 14:19:35 -0600

    create api client

---
+++ src/api/client.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+export const privateAPI = axios.create({
+  baseURL: process.env.BASE_API_PRIVATE_URL | "http://localhosh:3000",
+  timeout: 10_000,
+  headers: {
+    "Content-Type": "application/json"
+  }
+})
+
+export const publicAPI = axios.create({
+  baseURL: process.env.BASE_API_PUBLIC_URL | "http://localhosh:3000",
+  timeout: 10_000,
+  headers: {
+    "Content-Type": "application/json"
+  }
+})
